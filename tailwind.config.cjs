f744cf27c33fafd8d82c72ae4fe9cf924fd096c1
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-11 16:10:01 +0100

    add custom fonts and bg url

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Nunito Sans", ...defaultTheme.fontFamily.sans],
+        serif: ["Nunito Sans", ...defaultTheme.fontFamily.sans],
+        brand: ["Slackey", ...defaultTheme.fontFamily.sans],
+        heading: ["Fira Sans Extra Condensed", ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        hero: "linear-gradient(rgba(0,0,0,0.25), rgba(0,0,0,0.25)), url('/images/hero.jpeg')",
+      },
+    },
   },
   plugins: [],
 };
